341cdf35eba182cfa8f3d3d1a66f457335a8bcc3
drkop <satanatyn@yandex.ru>
2023-02-22 21:57:48 +0300

    1

---
+++ ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(" " + array[i]);
+    }
+}
+void FindKube(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        { 
+            array[i] = array[i]*array[i]*array[i];
+            Console.WriteLine(array[i]);
+        }
+    }
+}
+PrintArray(numbers);
+Console.WriteLine();
+FindKube(numbers);
